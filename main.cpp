b0099579d5debda3d6750bff3161c6c09ac1927e
Vivian Santos <vivianmilenapds@outlook.com>
2020-04-27 22:29:28 -0300

    Inicio da implementação de sections

--- main.cpp
+++ main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
+#include <matriz.h>
+#include<omp.h>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int tid = 0, n = 0;
+    cout << "Digite o valor de N: ";
+    cin >> n;
+    Matriz mat(n);
+
+    #pragma omp parallel shared(mat)
+    {
+        #pragma omp sections
+        {
+            #pragma omp section
+            {
+                mat.preencherMatriz();
+            }
+        }
+    }
+
+    cout << "\n";
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+
+    #pragma omp parallel num_threads(n) shared(mat)
+    {
+        tid=omp_get_thread_num();
+        #pragma omp for
+        for (int i=0; i< n; i++)
+        {
+            mat.multiplica(i);
+            cout << "Thread #" << tid << " executando na linha #" << i << endl;
+        }
+    }
+
+    Ticks[1] = clock();
+
+    #pragma omp parallel shared(mat)
+    {
+        #pragma omp sections
+        {
+            #pragma omp section
+            {
+                mat.imprimirMatriz();
+            }
+        }
+    }
+
+    double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    printf("\n\nTempo gasto: %g ms. \n\n", Tempo);
+
     return 0;
 }
---
+++ src/matriz.cpp
@@ -0,0 +1,91 @@
+#include "matriz.h"
+#include <iostream>
+#include <stdlib.h>
+
+/**Alunos: Leonardo Perales (C.C.), Pablo Alcantara (C.C.) e Vivian Santos (C.C.)**/
+
+using namespace std;
+
+Matriz::Matriz(int n)
+{
+    this->tamanho = n;
+    m1 = new int*[n];
+    m2 = new int*[n];
+    m3 = new int*[n];
+    for(int i = 0; i < n; i++)
+    {
+        m1[i] = new int[n];
+        m2[i] = new int[n];
+        m3[i] = new int[n];
+    }
+}
+
+void Matriz::preencherMatriz()
+{
+    int i, j;
+    for(i = 0; i < this->tamanho; i++)
+    {
+        for(j = 0; j < this->tamanho; j++)
+        {
+            this->m1[i][j] = rand() % 1001;
+            this->m2[i][j] = rand() % 1001;
+            this->m3[i][j] = 0;
+        }
+    }
+}
+
+void Matriz::imprimirMatriz()
+{
+    int i, j;
+
+    cout << "\nMatriz 1:";
+    for(i = 0; i < this->tamanho; i++)
+    {
+        cout << "" << endl;
+        for(j = 0; j < this->tamanho; j++)
+        {
+            cout << this->m1[i][j] <<  "\t";
+        }
+    }
+
+    cout << "\n\nMatriz 2:";
+    for(i = 0; i < this->tamanho; i++)
+    {
+        cout << "" << endl;
+        for(j = 0; j < this->tamanho; j++)
+        {
+            cout << this->m2[i][j] <<  "\t";
+        }
+    }
+
+    cout << "\n\nMatriz resultante:";
+    for(i = 0; i < this->tamanho; i++)
+    {
+        cout << "" << endl;
+        for(j = 0; j < this->tamanho; j++)
+        {
+            cout << this->m3[i][j] <<  "\t";
+        }
+    }
+
+    cout << "" << endl;
+}
+
+void Matriz::multiplica(int linha)
+{
+    int i, j;
+    for(i = 0; i < this->tamanho; i++)
+    {
+        for(j = 0; j < this->tamanho; j++)
+        {
+            this->m3[linha][i] += (this->m1[linha][j] * this->m2[j][i]);
+        }
+    }
+}
+
+Matriz::~Matriz()
+{
+    this->m1 = nullptr;
+    this->m2 = nullptr;
+    this->m3 = nullptr;
+}
